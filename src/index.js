1197b72f32408b63738f429d5b29ca5f45cefab8
Josh Choo <dev@joshuous.com>
2020-01-28 10:59:02 +0800

    create App, Editor and Preview component skeletons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Editor />
+        <Preview />
+      </div>
+    );
+  }
+}
+
+class Editor extends React.Component {
+  render() {
+    return <div className="editorWrap">Editor</div>;
+  }
+}
+
+class Preview extends React.Component {
+  render() {
+    return <div className="previewWrap">Preview</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
